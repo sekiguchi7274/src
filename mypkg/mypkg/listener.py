4f48af2b912f6ec7cd1c886882a6c2ec253f0468
sekiguchi <cyberakouxiang7274@gmail.com>
2024-11-21 23:41:21 +0900

    update talker & listener but there are in the middle

--- mypkg/mypkg/listener.py
+++ mypkg/mypkg/listener.py
@@ -1,17 +1,30 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import person
-
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("listener")
 
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg)
-
-
 def main():
-    pub = node.create_subscription(Person, "person", cb, 10)
-    rclpy.spin(node)
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "関口友雪"
+    future = client.call_async(req)
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                responce = future.result()
+            except:
+                node.get_logger().info('u r loser')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+
+            break
+
+    node.destory_node()
+    rclpy.shutdown()
--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -1,22 +1,20 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person,"person",10)
-n = 0
 
 
-def cb():
-    global n
-    msg = Person()
-    msg.name = "関口友雪"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+def cb(request, response):
+    if request.name == "関口友雪":
+        response.age = 19 
+    else:
+        response.age = 255
+
+    return response
 
 
 def main():
-    node.create_timer(0.5,cb)
+    srv = node.create_service(Query, "query", cb)
     rclpy.spin(node)
